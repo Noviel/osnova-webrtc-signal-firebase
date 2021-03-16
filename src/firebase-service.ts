85389f69f97cb674ba1848e4647b6a4696d15486
Alexander Snov <snov.contact@gmail.com>
2021-03-16 21:40:38 +0700

    feat: support for multiple firebase applications
    * `FirebaseServiceOptions.name` is used to define multiple applications

--- src/firebase-service.ts
+++ src/firebase-service.ts
@@ -1,5 +1,7 @@
 import firebase from 'firebase/app';
 
+export type { firebase };
+
 export interface FirebaseConfig {
   apiKey: string;
   projectId: string;
@@ -12,28 +14,32 @@ export interface FirebaseConfig {
 }
 
 export interface FirebaseServiceOptions {
+  name?: string;
   analytics?: boolean;
   firestore?: boolean;
   auth?: boolean;
 }
 
-export type FirebaseService = {
-  app: firebase.app.App;
-  firebase: typeof firebase;
-};
+export type FirebaseService = firebase.app.App;
+export type Firebase = typeof firebase;
 
 export function initializeFirebase(
   config: FirebaseConfig,
-  { analytics = true }: FirebaseServiceOptions = {}
+  { name = 'default', analytics = true }: FirebaseServiceOptions = {}
 ): FirebaseService {
-  const isApp = firebase.apps.length;
-  const app = !isApp ? firebase.initializeApp(config) : firebase.app();
+  // Check if app with provided name already exists. It is usefull for development environments with HMR
+  // to avoid Firebase errors about already initialized app.
+  const isAppInitialized = firebase.apps.length ? firebase.apps.find((app) => app.name === name) : undefined;
 
-  if (!isApp) {
+  // Get already initialized app or create a new one.
+  const app = isAppInitialized ? firebase.app(name) : firebase.initializeApp(config, name);
+
+  // Initialize required modules only if app is initialized for the first time.
+  if (!isAppInitialized) {
     if (analytics) {
       firebase.analytics();
     }
   }
 
-  return { app, firebase };
+  return app;
 }
